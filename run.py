165ec45825e7e046afaa5805633d7d2f6de1c869
nathansng <nathansng17@gmail.com>
2022-12-05 19:56:38 +0000

    Finish data pipeline in run.py

--- run.py
+++ run.py
@@ -10,17 +10,21 @@ from torch import optim
 from src.data import load_data
 from src.features import preprocessing, Scaler
 from src.models import TreNet, CNN, LSTM, train_models, setup
+from src.visualization import loss_visuals
 
 def main(targets):
     # Check device to store and run model on
     device = setup.find_device()
 
     if 'test' in targets:
-        print("Running program on test data")
-        targets += ['data', 'features', 'model']
+        print("Running program on test data\n")
+        targets += ['data', 'features', 'model', 'visual']
 
 
     if 'data' in targets:
+        print("Running data target\n")
+        print("Loading in trends data\n") 
+        
         # Load in data and separate into train, test sets
         with open('config/data_params.json') as f:
             data_cfg = json.load(f)
@@ -30,6 +34,9 @@ def main(targets):
 
 
     if 'features' in targets:
+        print("Running features target\n")
+        print("Preprocessing data...\n")
+        
         with open('config/feature_params.json') as f:
             feature_cfg = json.load(f)
 
@@ -47,16 +54,32 @@ def main(targets):
 
 
     if 'model' in targets:
+        print("Running model target\n")
+        print("Training model...\n")
+        
         with open('config/model_params.json') as f:
             model_cfg = json.load(f)
         with open('config/training_params.json') as f:
             training_cfg = json.load(f)
 
+        # Initialize model, loss, optimizer
         model = TreNet.TreNet(device=device, LSTM_params=model_cfg['LSTM'], CNN_params=model_cfg['CNN'], **model_cfg['TreNet']).to(device)
         loss_fn = nn.MSELoss()
         optimizer = optim.Adam(model.parameters(), lr=training_cfg['learning_rate'])
 
+        # Train model
         training_loss = train_models.train_loop(training_cfg['num_epochs'], [X_train_trend, X_train_points], y_train_trend.reshape(y_train_trend.shape[0], 2), model, loss_fn, optimizer, printout=True, record_loss=True)
+        
+    if 'visual' in targets: 
+        print("Running visual target\n")
+        print("Creating loss visualization...\n")
+        
+        with open('config/visual_params.json') as f:
+            visual_cfg = json.load(f)
+        
+        # Visualize loss per epoch and save plot 
+        loss_visuals.create_dir(visual_cfg['path'])
+        loss_visuals.visualize_loss(training_loss, **visual_cfg)
 
 
 if __name__ == '__main__':
