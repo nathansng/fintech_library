0dad0f8b8d48aeab260124bb2de9ca8230f7b7a7
Nathan N <nathansng17@gmail.com>
2023-03-13 15:09:32 -0700

    Add other files for documentation of visualizations

---
+++ src/visualization/README.md
@@ -0,0 +1,3 @@
+# Visualizations
+
+View training and validation losses after training a model using the training executor. Visualizations module can also be used to visualize any general 1 dimensional data, but is primarily used to plot losses per epoch.
--- src/visualization/loss_visuals.py
+++ src/visualization/loss_visuals.py
@@ -1,31 +1,49 @@
 import pandas as pd
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import seaborn as sns
 from pathlib import Path
 import os
 
 
-def create_dir(path): 
+def create_dir(path):
+    """Creates directories in path if it doesn't exist. Used to store saved visualization plots.
+
+    Args:
+        path (str): Relative path to create directories for
+
+    Returns:
+        None
     """
-    Creates folders for images
-    """
+
     Path(path).mkdir(parents=True, exist_ok=True)
 
-    
-def visualize_loss(data, title, xlabel, ylabel, savefig=False, path=None, img_name=None): 
+
+def visualize_loss(data, title, xlabel, ylabel, savefig=False, path=None, img_name=None):
+    """ Creates overlapping line plots for multiple sources of losses.
+
+    Args:
+        data (list or matrix): Losses to plot in a line plot, different sources of losses must be in different lists or rows
+        title (str): Title of plot
+        xlabel (str): x-axis label of plot
+        ylabel (str): y-axis label of plot
+        savefig (bool, optional): If *True*, saves a copy of the graph in a png file, must also specify path and img_name
+        path (str, optional): Path to store saved plot image in, doesn't work if savefig = *False*
+        img_name (str, optional): Name of saved plot image, doesn't work if savefig = *False*
+
+    Returns:
+        None
     """
-    Takes 1d or 2d data to visualize on line plot 
-    """
+
     sns.set()
     for i in data:
         plt.plot(i)
     plt.title(title)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
-    
-    if savefig: 
+
+    if savefig:
         path = os.path.join(path, img_name)
         plt.savefig(path)
         print("Plot saved at " + path)
-    
+
     plt.show()
