2bc1e3252ba2e9413e17838817b24ecc8e2a7fb2
rrichardtang <c3u2b4e@gmail.com>
2023-03-13 18:18:36 -0700

    Finished documentation for scaler.py

--- src/features/scaler.py
+++ src/features/scaler.py
@@ -4,26 +4,44 @@ import torch
 from sklearn.preprocessing import MinMaxScaler
 
 
-"""
-Scales multiple sources of data using Scaler class
-"""
-class MultiScaler(): 
-    def __init__(self, num_sources): 
+class MultiScaler():
+    """ Scales multiple sources of data using Scaler class
+
+    Args:
+        num_sources (int): Number of sources
+    """
+    def __init__(self, num_sources):
         self.Scalers = [Scaler() for i in range(num_sources)]
-       
+
     def fit_transform(self, data):
+        """ Transforms and fits data
+
+        Args:
+            data (list[tf tensor, Pandas DataFrame or Pandas Series]): Data to transform
+
+        Returns:
+            list[tf tensor]: Fitted and transformed data
+        """
         scale_data = []
-        for i in range(len(data)): 
-            if data[i] != None: 
+        for i in range(len(data)):
+            if data[i] != None:
                 scale_data.append(self.Scalers[i].fit_transform(data[i]))
-            else: 
+            else:
                 scale_data.append(None)
         return scale_data
-    
-    def inverse_transform(self, data): 
+
+    def inverse_transform(self, data):
+        """ Inverse transform data
+
+        Args:
+            data (list[tf tensor, Pandas DataFrame or Pandas Series]): Data to transform
+
+        Returns:
+            list[tf tensor]: Inverse-transformed data
+        """
         unscale_data = []
-        for i in range(len(data)): 
-            if data[i] != None: 
+        for i in range(len(data)):
+            if data[i] != None:
                 unscale_data.append(self.Scalers[i].inverse_transform(data[i]))
             else:
                 unscale_data.append(None)
@@ -35,10 +53,20 @@ Scales data using Sklearn's MinMaxScaler
 Generalized to accept tensors
 """
 class Scaler():
+    """ Scales data using Sklearn's MinMaxScaler. Generalized to accept tensors
+    """
     def __init__(self):
         self.scaler = MinMaxScaler()
 
     def fit_transform(self, data):
+        """ Transforms and fits data
+
+        Args:
+            data (tf tensor, Pandas DataFrame or Pandas Series): Data to fit and transform
+
+        Returns:
+            tf tensor: Fitted and transformed data
+        """
         # Check if data is tensor
         if type(data) == torch.Tensor:
             data = torch.Tensor.cpu(data).detach().numpy()
@@ -57,6 +85,14 @@ class Scaler():
         return torch.tensor(scaled_data, dtype=torch.float)
 
     def inverse_transform(self, data):
+        """ Inverse transform data
+
+        Args:
+            data (tf tensor, Pandas DataFrame or Pandas Series): Data to fit and transform
+
+        Returns:
+            tf tensor: Inverse-transformed data
+        """
         # Check if data is tensor
         if type(data) == torch.Tensor:
             data = torch.Tensor.cpu(data).detach().numpy()
